6c24034b0d9e7584932c65cf814dea37f3636f3f
Danila Churmanteev <churmantey@yandex.ru>
2024-08-25 23:01:41 +0300

    Добавлен функцонал работы с пользователями: получение списка, создание, обновление

---
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -0,0 +1,78 @@
+package ru.yandex.practicum.catsgram.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
+import ru.yandex.practicum.catsgram.exception.NotFoundException;
+import ru.yandex.practicum.catsgram.model.User;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final Map<Long, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        // проверяем выполнение необходимых условий
+        if (user.getEmail() == null || user.getEmail().isBlank()) {
+            throw new ConditionsNotMetException("Имейл должен быть указан");
+        } else if (emailInUse(user.getEmail())) {
+            throw new DuplicatedDataException("Этот имейл уже используется");
+        }
+        // формируем дополнительные данные
+        user.setId(getNextId());
+        user.setRegistrationDate(Instant.now());
+        // сохраняем новую публикацию в памяти приложения
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User newUser) {
+        // проверяем выполнение необходимых условий
+        if (newUser.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+        if (users.containsKey(newUser.getId())) {
+            if (newUser.getEmail() == null
+                    || newUser.getEmail().isBlank()) {
+                throw new ConditionsNotMetException("Имейл должен быть указан");
+            }
+            User oldUser = users.get(newUser.getId());
+            // если публикация найдена и все условия соблюдены, обновляем её содержимое
+            oldUser.setUsername(newUser.getUsername());
+            oldUser.setPassword(newUser.getPassword());
+            oldUser.setEmail(newUser.getEmail());
+            return oldUser;
+        }
+        throw new NotFoundException("Пользователь с id = " + newUser.getId() + " не найден");
+    }
+
+    // вспомогательный метод для генерации идентификатора нового юзера
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+    private boolean emailInUse(String email) {
+        return users.values()
+                .stream()
+                .anyMatch(user -> user.getEmail().equals(email));
+    }
+
+}
